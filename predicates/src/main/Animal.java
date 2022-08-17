b6dd0c8c944c00a1e6893d7cf366def1b9c1fbec
s-pat <sohanpatel96@gmail.com>
2022-08-16 21:41:35 -0400

    added inital predicate example

---
+++ predicates/src/main/Animal.java
@@ -0,0 +1,35 @@
+package main;
+
+public class Animal {
+    private String name;
+    private String type;
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    public Animal(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ predicates/src/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("test", "dog");
+        Animal animal2 = new Animal("test", "dog");
+        Animal animal3 = new Animal("test", "cat");
+        Animal animal4 = new Animal("test", "hamster");
+
+
+        List someList = List.of(animal1,animal2,animal3,animal4);
+
+       Predicate<Animal> filterOutDog = animal -> !animal.getType().equalsIgnoreCase("dog");
+
+       
+       someList.stream().filter(filterOutDog).forEach(System.out::println);
+
+      // someList = (List) someList.stream().filter(filterOutNumbersOver5).collect(Collectors.toList());
+        //System.out.println(someList);
+    }
+}
